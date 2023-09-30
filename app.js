07ac28d5d261de14b09ca6d428e0a3952b15bea7
Aashish Anand <anandaashish512@gmail.com>
2023-09-30 17:57:13 +0530

    MOdule-node.js

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    const url= req.url;
-    if(url==='/'){
+    const url = req.url;
+    const method = req.method;
+    if (url === '/') {
         res.write('<html>');
         res.write('<head><title>Enter Message</title></head>');
-        res.write('<body>Enter Message</body>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form><body>')
-        res.write('</html>');
-        return res.end(); 
+        res.write('<body>Enter Message');
+        res.write('<form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form>');
+        res.write('</body></html>');
+        return res.end();
     }
-   
+    if (url === '/message' && method === 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        return req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFile('message.txt', message, (err) => {
+                if (err) {
+                    console.error(err);
+                    return res.end('Error writing to file');
+                }
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
+        });
+    }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>My first Page</title></head>');
@@ -19,4 +40,6 @@ const server = http.createServer((req, res) => {
     res.end();
 });
 
-server.listen(3000);
+server.listen(3000, () => {
+    console.log('Server started on http://localhost:3000');
+});
