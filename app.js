0e8d0a7278e63e12bba78af749c6ed6071b52f5a
Aashish Anand <anandaashish512@gmail.com>
2023-10-03 16:47:53 +0530

    express lesson 1

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const http = require('http');
+const express=require('express');
 const routes=('./routes');
-
-const server = http.createServer(routes);
+const rexpress= require('express');
+const app=express()
+const server = http.createServer(app);
 
 server.listen(3000, () => {
     console.log('Server started on http://localhost:3000');
