0e259daf201b7a020f28f21896ec9931e3ffbcb9
Aashish Anand <anandaashish512@gmail.com>
2023-10-07 19:25:14 +0530

    Parsing incoming request

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const http = require('http');
-const express=require('express');
-const routes=('./routes');
-const rexpress= require('express');
-const app=express()
-const server = http.createServer(app);
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/add-product',(req, res, next) => {
+  
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</form>'); // corrected the response syntax
+});
+app.use('/product',(req,res,next)=>{
+    console.log(req.body)
+res.redirect('/');
+});
+app.use('/',(req, res, next) => {
+   
+    res.send('<h1>Hello from express</h1>'); 
+});
+
+
+
+const server = http.createServer(app); // create server using app
 
 server.listen(3000, () => {
     console.log('Server started on http://localhost:3000');
