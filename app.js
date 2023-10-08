34523a4aaa3d2adaca69e27551eb5cd3665309a3
Aashish Anand <anandaashish512@gmail.com>
2023-10-08 08:29:32 +0530

    file structuring

--- app.js
+++ app.js
@@ -1,26 +1,17 @@
 const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const app = express();
+const server = http.createServer(app);
 
-app.use(bodyParser.urlencoded({extended:false}));
+const adminRoutes = require('./router/admin');
+const ShopRoutes = require('./router/shop');
 
-app.use('/add-product',(req, res, next) => {
-  
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</form>'); // corrected the response syntax
-});
-app.use('/product',(req,res,next)=>{
-    console.log(req.body)
-res.redirect('/');
-});
-app.use('/',(req, res, next) => {
-   
-    res.send('<h1>Hello from express</h1>'); 
-});
+app.use(bodyParser.urlencoded({ extended: false }));
 
-
-
-const server = http.createServer(app); // create server using app
+app.use(adminRoutes);
+app.use(ShopRoutes);
 
 server.listen(3000, () => {
     console.log('Server started on http://localhost:3000');
